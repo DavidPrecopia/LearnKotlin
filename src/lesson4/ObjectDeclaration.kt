0362a1fbce7f3dc66f0744cfabd50fe38ac34628
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-06-18 15:51:40 -0400

    Added notes to ObjectDeclaration

--- src/lesson4/ObjectDeclaration.kt
+++ src/lesson4/ObjectDeclaration.kt
@@ -5,6 +5,9 @@ package lesson4
  * https://kotlinlang.org/docs/reference/object-declarations.html#object-declarations
  *
  * It cannot have a constructor (duh).
+ *
+ * You cannot declare objects inside of a functions (just like final in Java).
+ * You can declare an object inside other objects and non-inner class.
  */
 object ThisIsASingleton : FiskFarge {
     override fun farge() = "Singleton"
