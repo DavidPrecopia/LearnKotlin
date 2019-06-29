0d17e0557a9c272db338e9e6ab93bbfd2b2b1784
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-06-29 17:34:14 -0400

    Generic star projection

---
+++ src/lesson5/GenericsStarProjection.kt
@@ -0,0 +1,15 @@
+package lesson5
+
+/**
+ * Star represents some fixed type that you do not know.
+ *
+ * For example, List<*> VS List<Any>
+ * - With the former, it accept objects of any type, *but only that specific type*.
+ * It can accept String, then only accept String, nothing else.
+ * - With the latter, it can accept any object, String, Int, etc.
+ *
+ * Come back to this later - for review: https://stackoverflow.com/a/45521057
+ */
+fun main() {
+
+}
