aeefd2795c03a0bb6d9668cf79a55d522a98726c
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-06-29 16:29:45 -0400

    Generic method examples

---
+++ src/lesson5/GenericMethods.kt
@@ -0,0 +1,21 @@
+package lesson5
+
+fun main() {
+    // Invalid
+//    someGenericMethod(Driving<String>())
+
+    someGenericMethod(Driving<Mazda>())
+}
+
+private fun <T : AnotherCarClass> someGenericMethod(driving: Driving<T>) {
+    driving.someFoo()
+}
+
+
+open class AnotherCarClass(val title: String)
+
+class Mazda: AnotherCarClass("Mazda")
+
+class Driving<T> {
+    fun someFoo() = println("Driving class")
+}
