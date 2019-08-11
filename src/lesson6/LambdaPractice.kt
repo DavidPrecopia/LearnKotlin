a6872cf7674962d3fef858a34035146c5e41f799
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-08-11 18:54:17 -0400

    Inline function and practice passing higher order functions

--- src/lesson6/LambdaPractice.kt
+++ src/lesson6/LambdaPractice.kt
@@ -5,12 +5,9 @@ import lesson6.Directions.*
 fun main() {
     val game = Game()
 
-    // These call did not work until I added the parentheses.
-    game.addNorth()
-    game.addSouth()
-    game.addWest()
-    game.addEast()
-    game.end()
+    game.makeMove("n")
+    game.makeMove("w")
+    game.makeMove(null)
 }
 
 enum class Directions {
@@ -18,14 +15,27 @@ enum class Directions {
 }
 
 class Game(private val path: MutableList<Directions> = mutableListOf(START)) {
-    val addNorth = { path.add(NORTH) }
-    val addSouth = { path.add(SOUTH) }
-    val addEast = { path.add(EAST) }
-    val addWest = { path.add(WEST) }
-    val end = {
+    private val addNorth = { path.add(NORTH) }
+    private val addSouth = { path.add(SOUTH) }
+    private val addEast = { path.add(EAST) }
+    private val addWest = { path.add(WEST) }
+    private val end = {
         path.add(END)
         println("Game over: $path")
         path.clear()
         false
+    }
+
+    // To invoke `where`, you need to added parentheses to it OR call `.invoke()` on it.
+    private inline fun move(where: () -> Boolean) = where.invoke()
+
+    // I can simply pass the lambda function to the higher-order function because
+    // the higher-order function will invoke the lambda function.
+    fun makeMove(direction: String?) = when (direction) {
+        "n" -> move(addNorth)
+        "s" -> move(addSouth)
+        "e" -> move(addEast)
+        "w" -> move(addWest)
+        else -> move(end)
     }
 }
